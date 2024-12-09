a86c980754845685a48f23dbcb880a48013ef592
shlomoShalem <s0552288304@gmail.com>
2024-12-09 16:54:49 +0200

    ljhljh

--- g.js
+++ g.js
@@ -3,12 +3,12 @@ const a2 = document.querySelector(".a2")
 const a3 = document.querySelector(".a3")
 const a4 = document.querySelector(".a4")
 
-window.addEventListener("deviceorientation", e =>{
-    a1.textContent = "al: " + e.alpha
-    a2.textContent = "b: " + e.beta
-    a3.textContent = "g: " + e.gamma
-    a4.textContent = 'the north' + 360 - e.alpha
-})
+// window.addEventListener("deviceorientation", e =>{
+//     a1.textContent = "al: " + e.alpha
+//     a2.textContent = "b: " + e.beta
+//     a3.textContent = "g: " + e.gamma
+//     a4.textContent = 'the north' + 360 - e.alpha
+// })
 
 window.addEventListener("deviceorientation", (event) => {
     const alpha = event.alpha; // הכיוון ביחס לצפון מגנטי
