47fc0574e2cb382686d94dac125aae90ac7b491f
shlomoShalem <s0552288304@gmail.com>
2024-12-09 16:41:13 +0200

    kjg

---
+++ g.js
@@ -0,0 +1,10 @@
+const a1 = document.querySelector("a1")
+const a2 = document.querySelector("a2")
+const a3 = document.querySelector("a3")
+
+window.addEventListener("deviceorientation", e =>{
+    a1.textContent = e.alpha
+    a2.textContent = e.beta
+    a3.textContent = e.gamma
+    a4.textContent = 'the north' + e.gamma
+})
