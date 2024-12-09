6fbfefb6da12f58c048184d3056f943dc33e0f51
shlomoShalem <s0552288304@gmail.com>
2024-12-09 16:45:46 +0200

    sd

--- g.js
+++ g.js
@@ -1,10 +1,11 @@
-const a1 = document.querySelector("a1")
-const a2 = document.querySelector("a2")
-const a3 = document.querySelector("a3")
+const a1 = document.querySelector(".a1")
+const a2 = document.querySelector(".a2")
+const a3 = document.querySelector(".a3")
+const a4 = document.querySelector(".a4")
 
 window.addEventListener("deviceorientation", e =>{
-    a1.textContent = e.alpha
-    a2.textContent = e.beta
-    a3.textContent = e.gamma
+    a1.textContent = "al: " + e.alpha
+    a2.textContent = "b: " + e.beta
+    a3.textContent = "g: " + e.gamma
     a4.textContent = 'the north' + 360 - e.alpha
 })
