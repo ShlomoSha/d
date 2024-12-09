7d826cc8ac7d2076b25c118efa8ca269c4cf47a4
shlomoShalem <s0552288304@gmail.com>
2024-12-09 16:54:32 +0200

    yugouyg

--- g.js
+++ g.js
@@ -9,3 +9,10 @@ window.addEventListener("deviceorientation", e =>{
     a3.textContent = "g: " + e.gamma
     a4.textContent = 'the north' + 360 - e.alpha
 })
+
+window.addEventListener("deviceorientation", (event) => {
+    const alpha = event.alpha; // הכיוון ביחס לצפון מגנטי
+    const compassHeading = 360 - alpha; // חישוב הצפון האמיתי
+    console.log(`Compass Heading: ${compassHeading.toFixed(2)}°`);
+    a1.textContent = `al: ${compassHeading}` 
+});
